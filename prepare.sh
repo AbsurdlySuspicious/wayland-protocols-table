a46b061e763d813ee2d6206933f8ac81fa6e30ad
mfgm2 <repom2@riseup.net>
2025-05-06 05:35:40 +0400

    update prep script

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 set -eo pipefail
+
+log() {
+    echo "|" "$@" 1>&2
+}
+
+if [[ $SKIP_SUBMODULES != 1 ]]; then
+    log Updating submodules
+    git submodule update --init --depth=1
+fi
+
 pushd we-data
+log Compiling Typescript registry modules
 tsc compositor-registry.ts protocol-registry.ts
 popd
-rm -rfv web/logos
-cp -rv wayland-explorer/public/logos web/
+
+log Copying logo svgs
+logo_dst=web/logos
+mkdir -v "$logo_dst" || true
+rm -v "$logo_dst"/* || true
+cp -v wayland-explorer/public/logos/* "$logo_dst"/
+touch "$logo_dst"/.keep
+
+log Running data.json prepare script
+node prepare-data.js
+
+log Finished
